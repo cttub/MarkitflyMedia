810de49a423757b1d779320fc96fac8fe5088d4c
Christina Bui <uyen.buitie@gmail.com>
2023-08-29 00:56:34 -0500

    Pricing

--- src/Components/Price.js
+++ src/Components/Price.js
@@ -11,6 +11,12 @@ import check3 from '../Assets/Icons/Checks/third-check.svg';
 import empty3 from '../Assets/Icons/Checks/third-empty.svg';
 
 class Price extends Component {
+    scrollToConsultation = () => {
+        const consultAnchor = document.getElementById('consult-anchor');
+        if (consultAnchor) {
+            consultAnchor.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
     state = {  } 
     render() { 
         return (
@@ -19,11 +25,11 @@ class Price extends Component {
 
                 <section className='price-container'>
 
-                    <div className='price-box col-lg-3'>
+                    <div className='price-box col-lg-3 position-relative'>
                         <p className='title off-white primary'>Build My Presence</p>
                         <p className='primary-purple price text-center'>$2,579 + $250/month</p>
                         
-
+                    <div className='list-container'>
                         <ul className='benefit-list brock-regular'>
                             <li><img src={check1}/> Manage Online Ads</li>
                             <li><img src={check1}/> Monthly Consultations</li>
@@ -33,15 +39,17 @@ class Price extends Component {
                             <li className='not-benefit'><img src={empty1}/> Unlimited Requests</li> 
                             <li className='not-benefit'><img src={empty1}/> Unlimited Consultations</li>              
                         </ul>
-                        <div style={{display: "flex", padding: "14px"}}><button className='primary-p-btn margin-auto get-started'>Get Started</button></div>
+                    </div>
+                        <div style={{display: "flex", padding: "14px"}}><button onClick={this.scrollToConsultation} className='primary-p-btn margin-auto get-started'>Get Started</button></div>
 
                     </div>
 
-                    <div className='price-box col-lg-3'>
-                        <p className='title off-white secondary'>Build My Presence</p>
-                        <p className='secondary-purple price text-center'>$2,579 + $250/month</p>
+                    <div className='price-box col-lg-3 position-relative'>
+                        <p className='price-tag off-white box-shadow'>Most Popular!</p>
+                        <p className='title off-white secondary'>Start from Scratch</p>
+                        <p className='secondary-purple price text-center'>$4,879 + $300/month</p>
                         
-
+                    <div className='list-container'>
                         <ul className='benefit-list brock-regular'>
                             <li><img src={check2}/> Manage Online Ads</li>
                             <li><img src={check2}/> Monthly Consultations</li>
@@ -51,25 +59,27 @@ class Price extends Component {
                             <li className='not-benefit'><img src={empty2}/> Unlimited Requests</li> 
                             <li className='not-benefit'><img src={empty2}/> Unlimited Consultations</li>              
                         </ul>
-                        <div style={{display: "flex", padding: "14px"}}><button className='secondary-p-btn margin-auto get-started'>Get Started</button></div>
+                        </div>
+                        <div style={{display: "flex", padding: "14px"}}><button onClick={this.scrollToConsultation} className='secondary-p-btn margin-auto get-started'>Get Started</button></div>
 
                     </div>
 
-                    <div className='price-box col-lg-3'>
-                        <p className='title off-white third'>Build My Presence</p>
-                        <p className='secondary-purple price text-center'>$2,579 + $250/month</p>
+                    <div className='price-box col-lg-3 position-relative'>
+                        <p className='title off-white third'>Grow my Numbers</p>
+                        <p className='secondary-purple price text-center'>$5,079 + $450/month</p>
                         
-
+                    <div className='list-container'>
                         <ul className='benefit-list brock-regular'>
-                            <li><img src={check3}/> Manage Online Ads</li>
-                            <li><img src={check3}/> Monthly Consultations</li>
-                            <li><img src={check3}/> Website Design</li>
-                            <li><img src={check3}/> Website Development</li>
-                            <li><img src={check3}/> Website Hosting / Testing</li> 
-                            <li><img src={check3}/> Unlimited Requests</li> 
-                            <li><img src={check3}/> Unlimited Consultations</li>              
+                            <li><img src={check2}/> Manage Online Ads</li>
+                            <li><img src={check2}/> Monthly Consultations</li>
+                            <li><img src={check2}/> Website Design</li>
+                            <li><img src={check2}/> Website Development</li>
+                            <li><img src={check2}/> Website Hosting / Testing</li> 
+                            <li><img src={check2}/> Unlimited Requests</li> 
+                            <li><img src={check2}/> Unlimited Consultations</li>              
                         </ul>
-                        <div style={{display: "flex", padding: "14px"}}><button className='third-p-btn margin-auto get-started'>Get Started</button></div>
+                    </div>
+                        <div style={{display: "flex", padding: "14px"}}><button onClick={this.scrollToConsultation} className='third-p-btn margin-auto get-started'>Get Started</button></div>
 
                     </div>
 
